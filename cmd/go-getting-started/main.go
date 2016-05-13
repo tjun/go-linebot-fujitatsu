437b02d33220afa928d2a9dec50c2d642ea16a4c
Junichiro Takagi <t.junichiro@gmail.com>
2016-05-13 15:52:24 +0900

    import line bot sdk and write some command

--- cmd/go-getting-started/main.go
+++ cmd/go-getting-started/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
@@ -22,6 +25,38 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.POST("/callback", func(c *gin.Context) {
+		proxyURL, _ := url.Parse(os.Getenv("FIXIE_URL"))
+		client := &http.Client{
+			Transport: &http.Transport{Proxy: http.ProxyURL(proxyURL)},
+		}
+
+		bot, err := linebot.NewClient(123456789, "SECRET", "MID", linebot.WithHTTPClient(client))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		received, err := bot.ParseRequest(c.Request)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				fmt.Println(err)
+			}
+			return
+		}
+
+		for _, result := range received.Results {
+			content := result.Content()
+			if content != nil && content.IsMessage && content.ContentType == linebot.ContentTypeText {
+				text, err := content.TextContent()
+				res, err := bot.SendText([]string{content.From}, "OK "+text.Text)
+				if err != nil {
+					fmt.Println(res)
+				}
+			}
+		}
 	})
 
 	router.Run(":" + port)
